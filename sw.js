39d85facf1a554425139d4ca6f4ab46f0ff5d2ca
teramotodaiki <i101323@gmail.com>
2016-12-30 11:21:28 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,34 @@
 self.addEventListener('install', function(event) {
   // インストール処理
+  console.log('インストールできました!!');
 });
+
+self.addEventListener('fetch', function(event) {
+  console.log('プロキシできました!!!!');
+
+  const html = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Service Worker Test</title>
+        <script type="text/javascript">
+            alert('このアラートはキャッシュされたHTMLからのアレです！');
+        </script>
+    </head>
+    <body>
+        おめでとうございます！キャッシュです！
+    </body>
+</html>
+`;
+  const file = new Blob([html], {
+    type: 'text/html',
+  });
+  
+  event.respondWith(
+    Promise.resolve(
+      new Response(file)
+    )
+  );
+});
+
